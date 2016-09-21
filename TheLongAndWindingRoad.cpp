0962e00b26f420aeee490e006f8742598d7ab127
Andres Galaviz <andres.galaviz@gmail.com>
2016-09-21 12:37:16 -0500

    Parser works with variable declarations and const variable declaration

---
+++ TheLongAndWindingRoad.cpp
@@ -0,0 +1,49 @@
+bool bForceAzul09 = true;
+bool bForceAzul = true;
+bool ForceAzul09 = true;
+bool bforceAzul09 = true;
+bool bforceazul09 = true;
+bool i4 = true;
+bool bbforceazul09 = true;
+
+string sForceVoid09 = "a";
+string sForceAzul = "a";
+string ForceVoid09 = "a";
+string sforceAzul09 = "a";
+string sforceazul09 = "a";
+string ssforceazul09 = "a";
+
+char cForceAzul09 = 'a';
+char cForceAzul = 'a';
+char ForceAzul09 = 'a';
+char cforceAzul09 = 'a';
+char cforceazul09 = 'a';
+const char csforceazul09 = 'a';
+
+const int iForceAzul09 = 10;
+int iForceAzul = 10;
+int ForceAzul09 = 10;
+int iforceAzul09 = 10;
+int iforceazul09 = 10;
+int isforceazul09 = 10;
+
+double dForceAzul09 = 10.5;
+double ForceAzul = 10.5;
+double dorceAzul09 = 10.5;
+double dforceAzul09 = 10.5;
+double dforceazul09 = 10.5;
+double dsforceazul09 = 10.5;
+
+float fForceAzul09 = 10.5;
+float ForceAzul = 10.5;
+float forceAzul09 = 10.5;
+float fforceAzul09 = 10.5;
+float fforceazul09 = 10.5;
+float fsforceazul09 = 10.5;
+
+bool bForceAzul09 = true;
+bool bForceAzul = true;
+bool ForceAzul09 = true;
+bool bforceAzul09 = true;
+bool bforceazul09 = true;
+bool bbforceazul09 = true;
---
+++ tools.py
@@ -0,0 +1,3 @@
+reservedWords = {'and','and_eq','asm','auto','bitand','bitor','bool','break','case','catch','char','class','const','const_cast','continue','default','delete','do','double','dynamic_cast','else','enum','explicit','export','extern','false','float','for','friend','goto','if','inline','int','long','mutable','namespace','new','not','not_eq,''operator','or','or_eq','private','protected','public','register','reinterpret_cast','return','short','signed','sizeof','static','static_cast','string', 'struct','switch','template','this','throw','true','try','typedef','typeid','typename','union','unsigned','using','virtual','void','volatile','wchar_t','while','xor','xor_eq'}
+
+variableDefinitions = {'auto', 'bool', 'char', 'double', 'float', 'int', 'long', 'short', 'string'}
