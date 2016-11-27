f11ad71a806374ada477a12b01b07342fa9dc5af
Andres Galaviz <andres.galaviz@gmail.com>
2016-11-26 18:24:34 -0600

    Test cases and initial comments section

--- src/testfiles/Grade_a00819084_attempt_2016-08-22-12-04-02_fileNamePascalCaseIncorrect.cpp
+++ src/testfiles/Grade_a00819084_attempt_2016-08-22-12-04-02_fileNamePascalCaseIncorrect.cpp
@@ -1,3 +1,14 @@
+/*
+    fileNamePascalCaseIncorrect.cpp
+
+    This application calculates the hypothenuse
+    of a triangle with the length of the sides
+    given by the user
+
+    John Dow
+    2 / May / 2016
+    Version 1.0
+*/
 int main()
 {
     return 0;
