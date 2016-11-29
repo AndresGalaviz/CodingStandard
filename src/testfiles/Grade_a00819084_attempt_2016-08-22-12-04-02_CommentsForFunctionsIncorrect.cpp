afc864a22b8220236ac7b2990ebf16e3fdf2a147
Emilio Cornejo <emiliocornejo2@gmail.com>
2016-11-28 23:49:08 -0600

    Testfiles CommentsForFunctions correction

---
+++ src/testfiles/Grade_a00819084_attempt_2016-08-22-12-04-02_CommentsForFunctionsIncorrect.cpp
@@ -0,0 +1,23 @@
+/*  taxCalculation
+
+    It calculates the tax
+
+    Parameters:
+    iSalary the salary
+*/
+int taxCalculation(int iSalary) {
+    return 1;
+}
+
+int getOne(int iSalary) {
+    return 1;
+}
+
+/*
+    main
+    runs program
+*/
+int main()
+{
+    return 0;
+}
