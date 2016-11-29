9ec230b4290b8e83293c5a81930d87412bba7813
julio177 <julioc.aguilarv@gmail.com>
2016-11-29 11:46:42 -0600

    guarda files en folder con uuid

--- CodingStandardDjango/app/models.py
+++ CodingStandardDjango/app/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
-
+import uuid
 from django.db import models
 
 # Create your models here.
 class Document(models.Model):
-    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    docfile = models.FileField(upload_to='documents/%Y%m%d' + str(uuid.uuid4()))
